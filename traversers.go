ab3fbc1db177173da201f93f5ca4a301705a90f9
Jennifer Conner <2819667+phiryll@users.noreply.github.com>
2024-10-09 12:48:32 -0700

    Rename preOrderTraverse to preOrderRecurse, slightly clearer.

--- traversers.go
+++ traversers.go
@@ -8,9 +8,6 @@ import (
 // but I'm not sure that gains anything.
 // However, a stack[byte] can be rearranged to save memory.
 
-// TODO: return a sequence of things with both a path and a pruning method.
-// But only if a typical impl would need that to more quickly get to Bounds.Begin.
-
 // stack is a simple immutable singly-linked list used by preOrder.
 type stack[T any] struct {
 	value T
@@ -33,17 +30,17 @@ func (s *stack[T]) path() []T {
 
 func preOrder[T any](root T, adj func(T) iter.Seq[T]) iter.Seq[[]T] {
 	return func(yield func([]T) bool) {
-		preOrderTraverse(&stack[T]{root, nil, 1}, adj, yield)
+		preOrderRecurse(&stack[T]{root, nil, 1}, adj, yield)
 	}
 }
 
 // Returns a value of true if done (some yield has returned false).
-func preOrderTraverse[T any](s *stack[T], adj func(T) iter.Seq[T], yield func([]T) bool) bool {
+func preOrderRecurse[T any](s *stack[T], adj func(T) iter.Seq[T], yield func([]T) bool) bool {
 	if !yield(s.path()) {
 		return true
 	}
 	for node := range adj(s.value) {
-		if preOrderTraverse(s.with(node), adj, yield) {
+		if preOrderRecurse(s.with(node), adj, yield) {
 			return true
 		}
 	}
