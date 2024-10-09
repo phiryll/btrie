ba9f4e46ed025bbf1c05f54c5b0d63a80e7d3991
Jennifer Conner <2819667+phiryll@users.noreply.github.com>
2024-10-08 20:00:41 -0700

    Use string keys internally for reference impl.
    This removes the 1-3 bytes constraint on keys.

--- reference_test.go
+++ reference_test.go
@@ -2,7 +2,6 @@ package btrie_test
 
 import (
 	"bytes"
-	"fmt"
 	"iter"
 	"slices"
 
@@ -10,47 +9,17 @@ import (
 )
 
 func newReference() btrie.OrderedBytesMap[byte] {
-	return &reference{map[int32]byte{}}
+	return &reference{map[string]byte{}}
 }
-
-// TODO: expand to 4 bytes (truncated)
 
 // reference implements the OrderedBytesMap[byte] interface, but it is not a trie.
-// Keys must be 1-3 bytes, and values are exactly 1 byte.
 // This serves as an expected value to compare against an OrderedBytesMap implementation while testing.
 type reference struct {
-	// the high byte is 0, 1, or 2 if the key is 1, 2, or 3 bytes
-	m map[int32]byte
-}
-
-func refIndex(key []byte) int32 {
-	switch len(key) {
-	case 1:
-		return int32(key[0])
-	case 2:
-		return 0x01_00_00_00 | int32(key[0])<<8 | int32(key[1])
-	case 3:
-		return 0x02_00_00_00 | int32(key[0])<<16 | int32(key[1])<<8 | int32(key[2])
-	default:
-		panic(fmt.Sprintf("unsupported key length: %d", len(key)))
-	}
-}
-
-func refKey(index int32) []byte {
-	switch index & 0x03_00_00_00 {
-	case 0:
-		return []byte{byte(index)}
-	case 0x01_00_00_00:
-		return []byte{byte(index >> 8), byte(index)}
-	case 0x02_00_00_00:
-		return []byte{byte(index >> 16), byte(index >> 8), byte(index)}
-	default:
-		panic(fmt.Sprintf("invalid index: %d", index))
-	}
+	m map[string]byte
 }
 
 func (r *reference) Put(key []byte, value byte) (byte, bool) {
-	index := refIndex(key)
+	index := string(key)
 	prev, ok := r.m[index]
 	r.m[index] = value
 	if ok {
@@ -60,12 +29,12 @@ func (r *reference) Put(key []byte, value byte) (byte, bool) {
 }
 
 func (r *reference) Get(key []byte) (byte, bool) {
-	value, ok := r.m[refIndex(key)]
+	value, ok := r.m[string(key)]
 	return value, ok
 }
 
 func (r *reference) Delete(key []byte) (byte, bool) {
-	index := refIndex(key)
+	index := string(key)
 	value, ok := r.m[index]
 	delete(r.m, index)
 	return value, ok
@@ -78,7 +47,7 @@ func (r *reference) Range(bounds *Bounds) iter.Seq2[[]byte, byte] {
 	}
 	entries := []refEntry{}
 	for k, v := range r.m {
-		key := refKey(k)
+		key := []byte(k)
 		if !bounds.Contains(key) {
 			continue
 		}
